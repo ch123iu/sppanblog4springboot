efbe38bcf959c08461cc47a3d0ec2e0be6331abb
whoismy8023 <whoismy8023@163.com>
2017-03-17 13:09:56 +0800

    update

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,101 @@
+
+var tab;
+
+layui.config({
+	base: rootPath + '/js/component/',
+	version:new Date().getTime()
+}).use(['element', 'layer', 'navbar', 'tab'], function() {
+	var element = layui.element(),
+		$ = layui.jquery,
+		layer = layui.layer,
+		navbar = layui.navbar();
+		tab = layui.tab({
+			elem: '.admin-nav-card' //设置选项卡容器
+				,
+			//maxSetting: {
+			//	max: 5,
+			//	tipMsg: '只能开5个哇，不能再开了。真的。'
+			//},
+			contextMenu:true
+		});
+	//iframe自适应
+	$(window).on('resize', function() {
+		var $content = $('.admin-nav-card .layui-tab-content');
+		$content.height($(this).height() - 147);
+		$content.find('iframe').each(function() {
+			$(this).height($content.height());
+		});
+	}).resize();
+
+	//设置navbar
+	navbar.set({
+		spreadOne: true,
+		elem: '#admin-navbar-side',
+		/*data: navs,
+		cached:true,*/
+		url: rootPath + '/datas/nav.json'
+	});
+	//渲染navbar
+	navbar.render();
+	//监听点击事件
+	navbar.on('click(side)', function(data) {
+		tab.tabAdd(data.field);
+	});
+
+	$('.admin-side-toggle').on('click', function() {
+		var sideWidth = $('#admin-side').width();
+		if(sideWidth === 200) {
+			$('#admin-body').animate({
+				left: '0'
+			}); //admin-footer
+			$('#admin-footer').animate({
+				left: '0'
+			});
+			$('#admin-side').animate({
+				width: '0'
+			});
+		} else {
+			$('#admin-body').animate({
+				left: '200px'
+			});
+			$('#admin-footer').animate({
+				left: '200px'
+			});
+			$('#admin-side').animate({
+				width: '200px'
+			});
+		}
+	});
+	$('.admin-side-full').on('click', function() {
+		var docElm = document.documentElement;
+		//W3C  
+		if(docElm.requestFullscreen) {
+			docElm.requestFullscreen();
+		}
+		//FireFox  
+		else if(docElm.mozRequestFullScreen) {
+			docElm.mozRequestFullScreen();
+		}
+		//Chrome等  
+		else if(docElm.webkitRequestFullScreen) {
+			docElm.webkitRequestFullScreen();
+		}
+		//IE11
+		else if(elem.msRequestFullscreen) {
+			elem.msRequestFullscreen();
+		}
+		layer.msg('按Esc即可退出全屏');
+	});
+	
+	
+
+	//手机设备的简单适配
+	var treeMobile = $('.site-tree-mobile'),
+		shadeMobile = $('.site-mobile-shade');
+	treeMobile.on('click', function() {
+		$('body').addClass('site-mobile');
+	});
+	shadeMobile.on('click', function() {
+		$('body').removeClass('site-mobile');
+	});
+});
